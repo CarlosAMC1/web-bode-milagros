8e813ede9c0d6664aa1f760466686c7352bb37e3
CarlosAMC1 <carmaldonado4445@gmail.com>
2025-08-04 01:07:43 -0500

    Update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-// app/layout.tsx
+// src/app/layout.tsx
 import './globals.css';
+import { ReactNode } from 'react';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="es" className="scroll-smooth">
-      <body className="bg-neutral-50 font-sans antialiased">
-        {children}
-      </body>
+    <html lang="es">
+      <head />
+      <body className="bg-white text-gray-900">{children}</body>
     </html>
   );
 }
