fe68506622d462590083a5067b7b6c02f69e01bc
CarlosAMC1 <carmaldonado4445@gmail.com>
2025-08-04 01:03:09 -0500

    version 4 appdir

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true, // 👈 habilita App Router
+  },
 };
 
 export default nextConfig;
